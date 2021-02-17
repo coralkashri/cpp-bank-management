cca78606f2aa55cef6a5f23e0333cddc4f79f5f3
Koral Kashri <koralkashri@gmail.com>
2021-02-17 23:26:48 +0200

    Move make_regular/aggregate_filter to db_filter.
    Transform make_regular_filter to accept multiple fields for the filter.

--- src/db/filters/db_account_transactions_filter.cpp
+++ src/db/filters/db_account_transactions_filter.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include "../account_transactions_db_management.h"
 
+using namespace std::string_literals;
+
 using bsoncxx::builder::basic::kvp;
 using bsoncxx::builder::basic::sub_array;
 using bsoncxx::builder::basic::sub_document;
@@ -14,37 +16,31 @@ db_account_transactions_filter::db_account_transactions_filter(std::string trans
 }
 
 void db_account_transactions_filter::filter_by_id(document &filter, const std::string &transactions_hierarchy, const transaction_id &t_id) const {
-    filter.append(kvp(transactions_hierarchy + transactions_db_field_name, [t_id](sub_document transactions) {
-        transactions.append(kvp("$elemMatch", [&](sub_document $elem_match) {
-            $elem_match.append(kvp("transaction_name_id", [&](sub_document transaction_name_id) {
-                transaction_name_id.append(kvp("$eq", t_id.transaction_name));
-            }));
-            $elem_match.append(kvp("is_income", [&](sub_document is_income) {
-                is_income.append(kvp("$eq", t_id.is_income));
-            }));
-        }));
-    }));
+    db_filter::make_regular_filter(filter,
+                                   transactions_hierarchy + transactions_db_field_name,
+                                   std::pair{"transaction_name_id"s, t_id.transaction_name},
+                                   std::pair{"is_income"s, t_id.is_income});
 }
 
 void db_account_transactions_filter::filter_by_is_income(document &filter, const std::string &transactions_hierarchy,
                                                          bool is_income) const {
-    make_regular_filter(filter,
+    db_filter::make_regular_filter(filter,
                         transactions_hierarchy + transactions_db_field_name,
-                        "is_income", is_income);
+                                   std::pair{"is_income"s, is_income});
 }
 
 void db_account_transactions_filter::filter_archive_by_date(document &filter, const std::string &archive_hierarchy,
                                                             const boost::gregorian::date &date) const {
     std::string archive_field_name = "archive";
-    make_regular_filter(filter,
+    db_filter::make_regular_filter(filter,
                         archive_hierarchy + archive_field_name,
-                        "month", account_transactions_db_management::get_date_for_db(date));
+                                   std::pair{"month"s, account_transactions_db_management::get_date_for_db(date)});
 }
 
 void db_account_transactions_filter::aggregate_filter_by_is_income(document &filter,
                                                                    const std::string &transactions_hierarchy,
                                                                    bool is_income) const {
-    make_aggregate_filter(filter,
+    db_filter::make_aggregate_filter(filter,
                           transactions_hierarchy + transactions_db_field_name,
                           "is_income", is_income);
 }
@@ -53,7 +49,7 @@ void
 db_account_transactions_filter::aggregate_filter_archive_by_date(document &filter, const std::string &archive_hierarchy,
                                                                  const boost::gregorian::date &date) const {
     std::string archive_field_name = "archive";
-    make_aggregate_filter(filter,
+    db_filter::make_aggregate_filter(filter,
                           archive_hierarchy + archive_field_name,
                           "month", account_transactions_db_management::get_date_for_db(date));
 }
--- src/db/filters/db_filter.h
+++ src/db/filters/db_filter.h
@@ -2,6 +2,7 @@
 #define BANKMANAGEMENT_DB_FILTER_H
 
 #include <bsoncxx/builder/basic/document.hpp>
+#include <variant>
 
 class db_filter {
 public:
@@ -18,6 +19,57 @@ public:
         if (is_apply)
             concrete_filter.template apply<FilterType>(document_filter, field_hierarchy, std::forward<Args>(args)...);
         return *this;
+    }
+
+    /**
+     *
+     * @tparam Args
+     * @param filter
+     * @param array_hierarchy
+     * @param fields .first  -> Field name
+     *               .second -> Field param
+     */
+    template <typename ...Args>
+    static void make_regular_filter(bsoncxx::builder::basic::document &filter, const std::string &array_hierarchy,
+                                    std::pair<std::string, Args>&& ...fields) {
+        using bsoncxx::builder::basic::kvp;
+        using bsoncxx::builder::basic::sub_document;
+        using optional_variable_types = std::variant<std::string, int, bool, float, double>;
+
+        std::vector<std::pair<std::string, optional_variable_types>> fields_arr = {fields...};
+
+        filter.append(kvp(array_hierarchy, [&fields_arr](sub_document transactions) {
+            transactions.append(kvp("$elemMatch", [&](sub_document $elem_match) {
+                for (auto &field : fields_arr) {
+                    $elem_match.append(kvp(field.first, [&](sub_document transaction_name_id) {
+                        std::visit([&](auto &&arg) {
+                            transaction_name_id.append(kvp("$eq", arg));
+                        }, field.second);
+                    }));
+                }
+            }));
+        }));
+    }
+
+    template <typename ValueT>
+    static void make_aggregate_filter(bsoncxx::builder::basic::document &filter, const std::string &array_hierarchy,
+                               const std::string &field_name, ValueT &&val) {
+        using bsoncxx::builder::basic::kvp;
+        using bsoncxx::builder::basic::sub_document;
+        using bsoncxx::builder::basic::sub_array;
+
+        filter.append(kvp(array_hierarchy, [&](sub_document transactions) {
+            transactions.append(kvp("$filter", [&](sub_document $filter) {
+                $filter.append(kvp("input", "$" + array_hierarchy));
+                std::string it_name = "it";
+                $filter.append(kvp("as", it_name));
+                $filter.append(kvp("cond", [&](sub_document cond) {
+                    cond.append(kvp("$eq", [&](sub_array $eq) {
+                        $eq.append("$$" + it_name + "." + field_name, val);
+                    }));
+                }));
+            }));
+        }));
     }
 };
 
